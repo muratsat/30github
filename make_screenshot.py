b189f5abfd1a8c5841f2f17012c3eeda15316173
Murat <muratsat@users.noreply.github.com>
2025-02-07 11:44:38 +0600

    add screenshot script

---
+++ make_screenshot.py
@@ -0,0 +1,19 @@
+import argparse
+from playwright.sync_api import sync_playwright
+
+def take_screenshot(url: str, output_path: str):
+    with sync_playwright() as p:
+        browser = p.chromium.launch()
+        page = browser.new_page()
+        page.goto(url)
+        page.screenshot(path=output_path)
+        browser.close()
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--output", type=str, default="screenshot.png")
+args = parser.parse_args()
+
+if __name__ == '__main__':
+    # take_screenshot('https://github.com/muratsat/30github', output_path=args.output)
+    print(args.output)
+
